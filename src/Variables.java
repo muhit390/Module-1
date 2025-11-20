f532dd8d04f81f5c9de59eadde28861c3e260b73
muhit390 <muhit.bhuiyan@gmail.com>
2025-11-20 08:26:18 -0500

    Worked with reassigning values

--- src/Variables.java
+++ src/Variables.java
@@ -4,15 +4,26 @@ public class Variables {
         int dogCount = 94;
         int catCount = 112;
         int petCount = dogCount * catCount;
+        int tacos = 17;
+        int hotdogs = 23;
+        int total = tacos + hotdogs;
         System.out.println(petCount); // 206
 
-        int tacos = 12;
+        //int tacos = 12;
 
         System.out.println(tacos);
 
         tacos = 29;
 
         System.out.println(tacos);
+
+        tacos = 22;
+        System.out.println(total);
+        System.out.println(tacos);
+
+        total = tacos + hotdogs;
+
+        System.out.println(total);
     }
 
 }
