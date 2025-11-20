7d0916b180bce0200db5ebb1ecf3fa068d2f431c
muhit390 <muhit.bhuiyan@gmail.com>
2025-11-20 08:16:13 -0500

    Working with Java Variables

---
+++ src/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        int dogCount = 94;
+        int catCount = 112;
+        int petCount = dogCount * catCount;
+        System.out.println(petCount); // 206
+
+        int tacos = 12;
+
+        System.out.println(tacos);
+
+        tacos = 29;
+
+        System.out.println(tacos);
+    }
+
+}
